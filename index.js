91dc4a1e8c197c26842f88f8a31d4d41b158963c
Phineas <phineascoon@gmail.com>
2020-04-17 16:09:37 -0600

    moved css stuff to js

--- index.js
+++ index.js
@@ -1,3 +1,146 @@
+document.getElementById('red-1').style.marginTop = '-795px';
+document.getElementById('red-1').style.marginLeft = '-250px';
+document.getElementById('red-2').style.marginTop = '-795px';
+document.getElementById('red-2').style.marginLeft = '-50px';
+document.getElementById('red-3').style.marginTop = '-795px';
+document.getElementById('red-3').style.marginLeft = '150px';
+document.getElementById('red-4').style.marginTop = '-795px';
+document.getElementById('red-4').style.marginLeft = '350px';
+
+document.getElementById('red-5').style.marginTop = '-695px';
+document.getElementById('red-5').style.marginLeft = '-350px';
+document.getElementById('red-6').style.marginTop = '-695px';
+document.getElementById('red-6').style.marginLeft = '-150px';
+document.getElementById('red-7').style.marginTop = '-695px';
+document.getElementById('red-7').style.marginLeft = '50px';
+document.getElementById('red-8').style.marginTop = '-695px';
+document.getElementById('red-8').style.marginLeft = '250px';
+
+document.getElementById('red-9').style.marginTop = '-595px';
+document.getElementById('red-9').style.marginLeft = '-250px';
+document.getElementById('red-10').style.marginTop = '-595px';
+document.getElementById('red-10').style.marginLeft = '-50px';
+document.getElementById('red-11').style.marginTop = '-595px';
+document.getElementById('red-11').style.marginLeft = '150px';
+document.getElementById('red-12').style.marginTop = '-595px';
+document.getElementById('red-12').style.marginLeft = '350px';
+
+document.getElementById('blue-1').style.marginTop = '-295px';
+document.getElementById('blue-1').style.marginLeft = '-350px';
+document.getElementById('blue-2').style.marginTop = '-295px';
+document.getElementById('blue-2').style.marginLeft = '-150px';
+document.getElementById('blue-3').style.marginTop = '-295px';
+document.getElementById('blue-3').style.marginLeft = '50px';
+document.getElementById('blue-4').style.marginTop = '-295px';
+document.getElementById('blue-4').style.marginLeft = '250px';
+
+document.getElementById('blue-5').style.marginTop = '-195px';
+document.getElementById('blue-5').style.marginLeft = '-250px';
+document.getElementById('blue-6').style.marginTop = '-195px';
+document.getElementById('blue-6').style.marginLeft = '-50px';
+document.getElementById('blue-7').style.marginTop = '-195px';
+document.getElementById('blue-7').style.marginLeft = '150px';
+document.getElementById('blue-8').style.marginTop = '-195px';
+document.getElementById('blue-8').style.marginLeft = '350px';
+
+document.getElementById('blue-9').style.marginTop = '-95px';
+document.getElementById('blue-9').style.marginLeft = '-350px';
+document.getElementById('blue-10').style.marginTop = '-95px';
+document.getElementById('blue-10').style.marginLeft = '-150px';
+document.getElementById('blue-11').style.marginTop = '-95px';
+document.getElementById('blue-11').style.marginLeft = '50px';
+document.getElementById('blue-12').style.marginTop = '-95px';
+document.getElementById('blue-12').style.marginLeft = '250px';
+
+
+
+
+
+document.getElementById('sp-1').style.marginTop = '-795px';
+document.getElementById('sp-1').style.marginLeft = '-250px';
+document.getElementById('sp-2').style.marginTop = '-795px';
+document.getElementById('sp-2').style.marginLeft = '-50px';
+document.getElementById('sp-3').style.marginTop = '-795px';
+document.getElementById('sp-3').style.marginLeft = '150px';
+document.getElementById('sp-4').style.marginTop = '-795px';
+document.getElementById('sp-4').style.marginLeft = '350px';
+
+document.getElementById('sp-5').style.marginTop = '-695px';
+document.getElementById('sp-5').style.marginLeft = '-350px';
+document.getElementById('sp-6').style.marginTop = '-695px';
+document.getElementById('sp-6').style.marginLeft = '-150px';
+document.getElementById('sp-7').style.marginTop = '-695px';
+document.getElementById('sp-7').style.marginLeft = '50px';
+document.getElementById('sp-8').style.marginTop = '-695px';
+document.getElementById('sp-8').style.marginLeft = '250px';
+
+document.getElementById('sp-9').style.marginTop = '-595px';
+document.getElementById('sp-9').style.marginLeft = '-250px';
+document.getElementById('sp-10').style.marginTop = '-595px';
+document.getElementById('sp-10').style.marginLeft = '-50px';
+document.getElementById('sp-11').style.marginTop = '-595px';
+document.getElementById('sp-11').style.marginLeft = '150px';
+document.getElementById('sp-12').style.marginTop = '-595px';
+document.getElementById('sp-12').style.marginLeft = '350px';
+
+document.getElementById('sp-13').style.marginTop = '-495px';
+document.getElementById('sp-13').style.marginLeft = '-350px';
+document.getElementById('sp-14').style.marginTop = '-495px';
+document.getElementById('sp-14').style.marginLeft = '-150px';
+document.getElementById('sp-15').style.marginTop = '-495px';
+document.getElementById('sp-15').style.marginLeft = '50px';
+document.getElementById('sp-16').style.marginTop = '-495px';
+document.getElementById('sp-16').style.marginLeft = '250px';
+
+document.getElementById('sp-17').style.marginTop = '-395px';
+document.getElementById('sp-17').style.marginLeft = '-250px';
+document.getElementById('sp-18').style.marginTop = '-395px';
+document.getElementById('sp-18').style.marginLeft = '-50px';
+document.getElementById('sp-19').style.marginTop = '-395px';
+document.getElementById('sp-19').style.marginLeft = '150px';
+document.getElementById('sp-20').style.marginTop = '-395px';
+document.getElementById('sp-20').style.marginLeft = '350px';
+
+document.getElementById('sp-21').style.marginTop = '-295px';
+document.getElementById('sp-21').style.marginLeft = '-350px';
+document.getElementById('sp-22').style.marginTop = '-295px';
+document.getElementById('sp-22').style.marginLeft = '-150px';
+document.getElementById('sp-23').style.marginTop = '-295px';
+document.getElementById('sp-23').style.marginLeft = '50px';
+document.getElementById('sp-24').style.marginTop = '-295px';
+document.getElementById('sp-24').style.marginLeft = '250px';
+
+document.getElementById('sp-25').style.marginTop = '-195px';
+document.getElementById('sp-25').style.marginLeft = '-250px';
+document.getElementById('sp-26').style.marginTop = '-195px';
+document.getElementById('sp-26').style.marginLeft = '-50px';
+document.getElementById('sp-27').style.marginTop = '-195px';
+document.getElementById('sp-27').style.marginLeft = '150px';
+document.getElementById('sp-28').style.marginTop = '-195px';
+document.getElementById('sp-28').style.marginLeft = '350px';
+
+document.getElementById('sp-29').style.marginTop = '-95px';
+document.getElementById('sp-29').style.marginLeft = '-350px';
+document.getElementById('sp-30').style.marginTop = '-95px';
+document.getElementById('sp-30').style.marginLeft = '-150px';
+document.getElementById('sp-31').style.marginTop = '-95px';
+document.getElementById('sp-31').style.marginLeft = '50px';
+document.getElementById('sp-32').style.marginTop = '-95px';
+document.getElementById('sp-32').style.marginLeft = '250px';
+
+
+
+
+
 let allowMovement = (pawn) => {
-    
+    console.log(pawn)
+
+    for(let i = 1; i < 33; i++){
+        if(document.getElementById(pawn).style.marginTop === document.getElementById(`sp-${i}`).style.marginTop && document.getElementById(pawn).style.marginLeft === document.getElementById(`sp-${i}`).style.marginLeft){
+            document.getElementById(`sp-${i}`).style.visibility = 'visible'
+            console.log(document.getElementById(`sp-${i}`).style.visibility)
+            console.log(document.getElementById(pawn).style.marginTop)
+            console.log(document.getElementById(`sp-${i}`).style.marginTop)
+        }
+    }
 }
