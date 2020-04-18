736d1ca75a573a92a2baa7e4fe1b1dcb604a055e
Phineas <phineascoon@gmail.com>
2020-04-17 20:43:44 -0600

    stuck but ill figure it out

--- index.js
+++ index.js
@@ -58,89 +58,152 @@ document.getElementById('blue-12').style.marginLeft = '250px';
 
 document.getElementById('sp-1').style.marginTop = '-795px';
 document.getElementById('sp-1').style.marginLeft = '-250px';
+document.getElementById('sp-1').style.visibility = 'hidden';
 document.getElementById('sp-2').style.marginTop = '-795px';
 document.getElementById('sp-2').style.marginLeft = '-50px';
+document.getElementById('sp-2').style.visibility = 'hidden';
 document.getElementById('sp-3').style.marginTop = '-795px';
 document.getElementById('sp-3').style.marginLeft = '150px';
+document.getElementById('sp-3').style.visibility = 'hidden';
 document.getElementById('sp-4').style.marginTop = '-795px';
 document.getElementById('sp-4').style.marginLeft = '350px';
+document.getElementById('sp-4').style.visibility = 'hidden';
 
 document.getElementById('sp-5').style.marginTop = '-695px';
 document.getElementById('sp-5').style.marginLeft = '-350px';
+document.getElementById('sp-5').style.visibility = 'hidden';
 document.getElementById('sp-6').style.marginTop = '-695px';
 document.getElementById('sp-6').style.marginLeft = '-150px';
+document.getElementById('sp-6').style.visibility = 'hidden';
 document.getElementById('sp-7').style.marginTop = '-695px';
 document.getElementById('sp-7').style.marginLeft = '50px';
+document.getElementById('sp-7').style.visibility = 'hidden';
 document.getElementById('sp-8').style.marginTop = '-695px';
 document.getElementById('sp-8').style.marginLeft = '250px';
+document.getElementById('sp-8').style.visibility = 'hidden';
 
 document.getElementById('sp-9').style.marginTop = '-595px';
 document.getElementById('sp-9').style.marginLeft = '-250px';
+document.getElementById('sp-9').style.visibility = 'hidden';
 document.getElementById('sp-10').style.marginTop = '-595px';
 document.getElementById('sp-10').style.marginLeft = '-50px';
+document.getElementById('sp-10').style.visibility = 'hidden';
 document.getElementById('sp-11').style.marginTop = '-595px';
 document.getElementById('sp-11').style.marginLeft = '150px';
+document.getElementById('sp-11').style.visibility = 'hidden';
 document.getElementById('sp-12').style.marginTop = '-595px';
 document.getElementById('sp-12').style.marginLeft = '350px';
+document.getElementById('sp-12').style.visibility = 'hidden';
 
 document.getElementById('sp-13').style.marginTop = '-495px';
 document.getElementById('sp-13').style.marginLeft = '-350px';
+document.getElementById('sp-13').style.visibility = 'hidden';
 document.getElementById('sp-14').style.marginTop = '-495px';
 document.getElementById('sp-14').style.marginLeft = '-150px';
+document.getElementById('sp-14').style.visibility = 'hidden';
 document.getElementById('sp-15').style.marginTop = '-495px';
 document.getElementById('sp-15').style.marginLeft = '50px';
+document.getElementById('sp-15').style.visibility = 'hidden';
 document.getElementById('sp-16').style.marginTop = '-495px';
 document.getElementById('sp-16').style.marginLeft = '250px';
+document.getElementById('sp-16').style.visibility = 'hidden';
 
 document.getElementById('sp-17').style.marginTop = '-395px';
 document.getElementById('sp-17').style.marginLeft = '-250px';
+document.getElementById('sp-17').style.visibility = 'hidden';
 document.getElementById('sp-18').style.marginTop = '-395px';
 document.getElementById('sp-18').style.marginLeft = '-50px';
+document.getElementById('sp-18').style.visibility = 'hidden';
 document.getElementById('sp-19').style.marginTop = '-395px';
 document.getElementById('sp-19').style.marginLeft = '150px';
+document.getElementById('sp-19').style.visibility = 'hidden';
 document.getElementById('sp-20').style.marginTop = '-395px';
 document.getElementById('sp-20').style.marginLeft = '350px';
+document.getElementById('sp-20').style.visibility = 'hidden';
 
 document.getElementById('sp-21').style.marginTop = '-295px';
 document.getElementById('sp-21').style.marginLeft = '-350px';
+document.getElementById('sp-21').style.visibility = 'hidden';
 document.getElementById('sp-22').style.marginTop = '-295px';
 document.getElementById('sp-22').style.marginLeft = '-150px';
+document.getElementById('sp-22').style.visibility = 'hidden';
 document.getElementById('sp-23').style.marginTop = '-295px';
 document.getElementById('sp-23').style.marginLeft = '50px';
+document.getElementById('sp-23').style.visibility = 'hidden';
 document.getElementById('sp-24').style.marginTop = '-295px';
 document.getElementById('sp-24').style.marginLeft = '250px';
+document.getElementById('sp-24').style.visibility = 'hidden';
 
 document.getElementById('sp-25').style.marginTop = '-195px';
 document.getElementById('sp-25').style.marginLeft = '-250px';
+document.getElementById('sp-25').style.visibility = 'hidden';
 document.getElementById('sp-26').style.marginTop = '-195px';
 document.getElementById('sp-26').style.marginLeft = '-50px';
+document.getElementById('sp-26').style.visibility = 'hidden';
 document.getElementById('sp-27').style.marginTop = '-195px';
 document.getElementById('sp-27').style.marginLeft = '150px';
+document.getElementById('sp-27').style.visibility = 'hidden';
 document.getElementById('sp-28').style.marginTop = '-195px';
 document.getElementById('sp-28').style.marginLeft = '350px';
+document.getElementById('sp-28').style.visibility = 'hidden';
 
 document.getElementById('sp-29').style.marginTop = '-95px';
 document.getElementById('sp-29').style.marginLeft = '-350px';
+document.getElementById('sp-29').style.visibility = 'hidden';
 document.getElementById('sp-30').style.marginTop = '-95px';
 document.getElementById('sp-30').style.marginLeft = '-150px';
+document.getElementById('sp-30').style.visibility = 'hidden';
 document.getElementById('sp-31').style.marginTop = '-95px';
 document.getElementById('sp-31').style.marginLeft = '50px';
+document.getElementById('sp-31').style.visibility = 'hidden';
 document.getElementById('sp-32').style.marginTop = '-95px';
 document.getElementById('sp-32').style.marginLeft = '250px';
+document.getElementById('sp-32').style.visibility = 'hidden';
 
 
 
 
 
 let allowMovement = (pawn) => {
-    console.log(pawn)
+    let x = document.getElementById(pawn).style.marginTop.split('')
+    x.pop()
+    x.pop()
+    x = x.join('')
+    let xplus = +x+100
+    xplus+='px'
+    let xminus = +x-100
+    xminus+='px'
+
+
+
+    let y = document.getElementById(pawn).style.marginLeft.split('')
+    y.pop()
+    y.pop()
+    y = y.join('')
+    let yplus = +y+100
+    yplus+='px'
+    let yminus = +y-100
+    yminus+='px'
+
+
 
     for(let i = 1; i < 33; i++){
-        if(document.getElementById(pawn).style.marginTop === document.getElementById(`sp-${i}`).style.marginTop && document.getElementById(pawn).style.marginLeft === document.getElementById(`sp-${i}`).style.marginLeft){
-            document.getElementById(`sp-${i}`).style.visibility = 'visible'
-            console.log(document.getElementById(`sp-${i}`).style.visibility)
-            console.log(document.getElementById(pawn).style.marginTop)
-            console.log(document.getElementById(`sp-${i}`).style.marginTop)
+        if(xminus === document.getElementById(`sp-${i}`).style.marginTop && yminus === document.getElementById(`sp-${i}`).style.marginLeft){
+
+            //    !!!! THIS BIT IS WEIRD.  TRYNA FIGURE IT OUT !!!!
+            //
+            // for(let j = 1; j < 13; j++){
+            //     if(xminus === document.getElementById(`blue-${j}`).style.marginTop && yminus === document.getElementById(`blue-${j}`).style.marginLeft){
+            //         document.getElementById(`sp-${i}`).style.visibility = 'visible'
+            //     }
+            
+            // }
         }
+
+        // if(xminus === document.getElementById(`sp-${i}`).style.marginTop && yplus === document.getElementById(`sp-${i}`).style.marginLeft){
+        //     document.getElementById(`sp-${i}`).style.visibility = 'visible'
+            
+        // }
+            
     }
 }
